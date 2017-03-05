6a754fabeb46fcb9a8f1526a3cc01c757848c8ac
jianzhichun <zzchun12826@gmail.com>
2017-03-05 18:00:24 +0800

    formate project

--- src/main/java/org/chun/spring/boot/redis/query/config/RedisQueryConfig.java
+++
@@ -1,8 +0,0 @@
-package org.chun.spring.boot.redis.query.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "redis.query")
-public class RedisQueryConfig {
-	
-}
