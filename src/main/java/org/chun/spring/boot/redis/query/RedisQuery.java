1806e30a29ef45daa30ce3d78d51ca93fb17b3ef
jianzhichun <zzchun12826@gmail.com>
2017-03-05 13:02:52 +0800

    post-construction

--- src/main/java/org/chun/spring/boot/redis/query/RedisQuery.java
+++ src/main/java/org/chun/spring/boot/redis/query/RedisQuery.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface RedisQuery {
@@ -17,9 +18,26 @@ public @interface RedisQuery {
 	
 	/**
 	 * True will not invoke java method
-	 * <p>default is false
+	 * <p>default is true
+	 * <p>false logic need TODO
 	 * @return
 	 */
-	boolean replace() default false;
+	boolean replace() default true;
+	
+	/**
+	 * The KEYS for lua script
+	 * <p> KEYS from method-params[0:0+keysCount()]
+	 * @return
+	 */
+	int keysCount() default 0;
+	
+	/**
+	 * The ARGS for lua script
+	 * <p> ARGS from method-params[keys()+1:keys()+1+argsCount()]
+	 * <p> None if args() == 0
+	 * @return
+	 */
+	int argsCount() default 0;
+	
 	
 }
--- src/main/java/org/chun/spring/boot/redis/query/config/RedisQueryAutoConfiguration.java
+++ src/main/java/org/chun/spring/boot/redis/query/config/RedisQueryAutoConfiguration.java
@@ -1,6 +1,15 @@
 package org.chun.spring.boot.redis.query.config;
 
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import javax.annotation.PostConstruct;
 
 import org.aopalliance.aop.Advice;
@@ -13,10 +22,16 @@ import org.springframework.aop.Pointcut;
 import org.springframework.aop.support.AbstractPointcutAdvisor;
 import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.ScriptExecutor;
 
-@SuppressWarnings("serial")
+
+@SuppressWarnings({"serial","rawtypes","unchecked","unused"})
 @Configuration
 @EnableConfigurationProperties(RedisQueryConfig.class)
 public class RedisQueryAutoConfiguration extends AbstractPointcutAdvisor {
@@ -25,8 +40,11 @@ public class RedisQueryAutoConfiguration extends AbstractPointcutAdvisor {
 	private Pointcut pointcut;
     private Advice advice;
 	
-    @Autowired
+	@Autowired
     private RedisQueryConfig redisQueryConfig;
+    
+	@Autowired
+    private RedisTemplate redisTemplate;
     
     @PostConstruct
     public void init() {
@@ -46,12 +64,32 @@ public class RedisQueryAutoConfiguration extends AbstractPointcutAdvisor {
 		return this.advice;
 	}
 	
+	
 	class CloudsQueryMethodInterceptor implements MethodInterceptor{
 		
 		@Override
 		public Object invoke(MethodInvocation invocation) throws Throwable {
-			System.out.println(redisQueryConfig);
-			return null;
+			if(null == redisTemplate){
+				logger.warn("RedisTemplate is not exist, will do method logic");
+				return invocation.proceed();
+			}
+			Method method = invocation.getMethod();
+			RedisQuery redisQuery = method.getDeclaredAnnotation(RedisQuery.class);
+			if(!redisQuery.replace()){
+				//TODO
+			}
+			Class<?> resultType = method.getReturnType();
+			Object[] arguments = invocation.getArguments();
+			//keys
+			int keysCount = redisQuery.keysCount();
+			List<Object> keys = new ArrayList<Object>(keysCount);
+			Collections.addAll(keys, Arrays.copyOfRange(arguments, 0, keysCount));
+			//args
+			int argsCount = redisQuery.argsCount();
+			Object[] args = ( argsCount == 0 && keysCount < arguments.length -1 ) ? 
+					null : Arrays.copyOfRange(arguments, keysCount + 1, argsCount + keysCount + 1);
+			Object result = redisTemplate.execute(new DefaultRedisScript(redisQuery.lua(), resultType), keys, args);
+	        return result;
 		}
 		
 	}
